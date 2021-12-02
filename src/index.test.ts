8809e5e635275e76e981dc00ffa904d1bb7702a1
Infinituum17 <riccardomele1706@gmail.com>
2021-12-02 12:27:50 +0100

    📰 Refactoring

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,32 +2,34 @@ import { readInputFile } from "./utils";
 
 const debug = false;
 
-test("Day 1 - Sonar Sweep: Part One", async () => {
-  const module = await import("../day-1-sonar-sweep/part-one");
-  const resolver = module.default;
+describe("Day One", () => {
+  test("Sonar Sweep: Part One", async () => {
+    const module = await import("../day-1-sonar-sweep/part-one");
+    const resolver = module.default;
 
-  // Test cases
-  const testInput = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263";
+    // Test cases
+    const testInput = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263";
 
-  expect(resolver(testInput, debug)).toBe(7);
+    expect(resolver(testInput, debug)).toBe(7);
 
-  // Solution (1342)
-  const input = await readInputFile("./day-1-sonar-sweep/input.txt");
+    // Solution (1342)
+    const input = await readInputFile("./day-1-sonar-sweep/input.txt");
 
-  expect(resolver(input)).toBe(1342);
-});
+    expect(resolver(input)).toBe(1342);
+  });
 
-test("Day 1 - Sonar Sweep: Part Two", async () => {
-  const module = await import("../day-1-sonar-sweep/part-two");
-  const resolver = module.default;
+  test("Sonar Sweep: Part Two", async () => {
+    const module = await import("../day-1-sonar-sweep/part-two");
+    const resolver = module.default;
 
-  // Test cases
-  const testInput = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263";
+    // Test cases
+    const testInput = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263";
 
-  expect(resolver(testInput, true)).toBe(5);
+    expect(resolver(testInput, true)).toBe(5);
 
-  // Solution (...)
-  const input = await readInputFile("./day-1-sonar-sweep/input.txt");
+    // Solution (...)
+    const input = await readInputFile("./day-1-sonar-sweep/input.txt");
 
-  expect(resolver(input)).toBe(1378);
+    expect(resolver(input)).toBe(1378);
+  });
 });
