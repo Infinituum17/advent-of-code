fcd0bdac177752c3ba793ac1a65a5aeaf858e0b7
Infinituum17 <riccardomele1706@gmail.com>
2021-12-08 17:45:14 +0100

    🎉 Day 08: Part 1

---
+++ day-08/part-one.ts
@@ -0,0 +1,44 @@
+export default function resolver(input: string[]) {
+  const [signalPatterns, outputValues] = parseMessage(input);
+
+  return getUniqueDigits(outputValues).length;
+}
+
+export function parseMessage(input: string[]) {
+  const signalPatterns = [];
+  const outputValues = [];
+
+  for (let i = 0; i < input.length; i++) {
+    const [signalPatternsRaw, outputValueRaw] = input[i].split("|");
+    signalPatterns.push(signalPatternsRaw.split(/\s/gim));
+    outputValues.push(outputValueRaw.split(/\s/gim));
+  }
+
+  return [signalPatterns, outputValues];
+}
+
+export function getUniqueDigits(outputValues: string[][]) {
+  const uniqueDigits: digitAssociation[] = [];
+
+  outputValues.forEach((line) => {
+    line.forEach((outputValue) => {
+      switch (outputValue.length) {
+        case 2:
+          uniqueDigits.push({ value: outputValue, digit: 1 });
+          break;
+        case 3:
+          uniqueDigits.push({ value: outputValue, digit: 7 });
+          break;
+        case 4:
+          uniqueDigits.push({ value: outputValue, digit: 4 });
+          break;
+        case 7:
+          uniqueDigits.push({ value: outputValue, digit: 8 });
+      }
+    });
+  });
+
+  return uniqueDigits;
+}
+
+type digitAssociation = { value: string; digit: number };
---
+++ src/day-08.test.ts
@@ -0,0 +1,19 @@
+import { readLines } from "./utils";
+import resolver1 from "../day-08/part-one";
+
+const testInput =
+  "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\nedbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\nfgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\nfbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\naecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\nfgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\ndbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\nbdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\negadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\ngcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce".split(
+    /\n/gim
+  );
+
+describe("Day 8: Seven Segment Search", () => {
+  test("Part 1", async () => {
+    // Test cases
+    expect(resolver1(testInput)).toBe(26);
+
+    const input = await readLines("./day-08/input.txt");
+
+    // Solution (318)
+    expect(resolver1(input)).toBe(318);
+  });
+});
